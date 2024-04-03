c9f8887254a09827638067c07e1566a6bf012d7a
vaxo-grigolishvili <60270002+vaxo-grigolishvili@users.noreply.github.com>
2024-04-04 00:33:26 +0400

    done

---
+++ js/index.js
@@ -0,0 +1,48 @@
+let x=100;
+let result=undefined;
+if(x>20){
+     result= "x მეტია ოცზე";
+}
+else{
+    result=" შეცდომა";
+}
+console.log(result);
+ 
+let y=100;
+let z=undefined;
+if(y<50){
+     z= "y ნაკლებია 50 ზე";
+}
+else{
+    z=" შეცდომა";
+}
+console.log(z);
+
+let w=100;
+let answer=w<50 ? "w ნაკლებია 50 ზე" : "შეცდომა";
+console.log(answer);
+let Q=100;
+let sum=Q>50 ? "Q მეტია 20 ზე" : "შეცდომა";
+console.log(sum);
+ 
+let p="mariami";
+if(p="mariami"){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+
+switch (p){
+    case 0:
+        if( p="mariami"){
+            console.log(true);
+        }
+        else{
+            console.log(false);
+        }
+     break;
+}
+let number=10;
+ console.log(String(number));
+
